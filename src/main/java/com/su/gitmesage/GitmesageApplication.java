71da0397defa9c68608d2f228fd39556dd5f9ba2
suzq <suzq@broada.com>
2020-10-10 16:56:17 +0800

    init

---
+++ src/main/java/com/su/gitmesage/GitmesageApplication.java
@@ -0,0 +1,13 @@
+package com.su.gitmesage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitmesageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitmesageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/su/gitmesage/GitmesageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.su.gitmesage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitmesageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
