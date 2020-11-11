691e0fb05dd59adff1f7abb925a5fccbd1f0d338
suzq <suzq@broada.com>
2020-11-11 10:35:33 +0800

    build:使用maven-assembly打包

--- bin/startup.bat
+++ bin/startup.bat
@@ -1,4 +1,4 @@
-set CLASSPATH="./module/*;./lib/*"
+set CLASSPATH="./lib/*"
 set MAIN_CLASS=com.su.gitmesage.GitmesageApplication
 set DEBUG_PORT=9000
 set DEBUG_OPTS="-Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=%DEBUG_PORT%"
